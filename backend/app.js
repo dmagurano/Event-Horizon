39e252eabb78125aa1a071d60be2f12eb1120eae
Daniele Magurano <daniele.magurano@aalto.fi>
2017-12-01 17:52:04 +0200

    Image uploading implemented

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
 , bodyParser = require('body-parser');
 const app = express();
+const Multer = require('multer');
+const format = require('util').format;
 //const router = express.Router();
 
 process.title = "mcc";
 
+const googleStorage = require('@google-cloud/storage');
 var admin = require("firebase-admin");
 var crypto = require('crypto');
+
+/* FIREBASE REALTIME DB CONFIG */
 
 var serviceAccount = require("./mcc-fall-2017-g04-35ff1cc15d6d.json");
 
@@ -21,6 +26,25 @@ var ref = db.ref("/");
 ref.once("value", function(snapshot) {
   //console.log(snapshot.val());
 });
+
+/* IMAGE UPLOAD CONFIG */
+
+// Multer is required to process file uploads and make them available via
+// req.files.
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 50 * 1024 * 1024 // no larger than 50mb, you can change as needed.
+  }
+});
+
+const storage = googleStorage({
+  projectId: "mcc-fall-2017-g04",
+  keyFilename: "./mcc-fall-2017-g04-35ff1cc15d6d.json"
+});
+
+// Google Cloud Storage Bucket
+const bucket = storage.bucket("mcc-fall-2017-g04.appspot.com");
 
 app.use(bodyParser.json());
 
@@ -156,6 +180,77 @@ app.post('/leave', (req, res) => {
     res.send(req.body)
 }
 });
+
+/* IMAGE UPLOAD */ 
+
+/**
+ * Adding new file to the storage
+ */
+app.post('/upload', multer.single('file'), (req, res) => {
+        console.log('Upload Image');
+        if(req.body.idToken != undefined && req.body.groupId != undefined){
+        // admin.auth().verifyIdToken(req.body.idToken)
+        // .then(function(decodedToken) {
+
+            let file = req.file;
+            if (file) {
+            uploadImageToStorage(file, req.body.groupId).then((success) => {
+              res.status(200).send({
+                status: 'success'
+              });
+            }).catch((error) => {
+              console.error(error);
+              res.status(500).send("Failure in uploading the image")
+            });
+            } else {
+            res.status(400).send("No image was uploaded")
+            }
+        // }).catch(function(error) {
+        //     res.send(error.message)
+        // });
+        } else {
+            res.send(req.body)
+        }
+});
+
+/**
+ * Upload the image file to Google Storage
+ * @param {File} file object that will be uploaded to Google Storage
+ */
+const uploadImageToStorage = (file, groupId) => {
+  let prom = new Promise((resolve, reject) => {
+    if (!file) {
+      reject('No image file');
+    }
+
+    if (file.mimetype != "image/jpeg") {
+        reject("Only JPG images allowed")
+    }
+    let newFileName = `/${groupId}/fullres/${file.originalname}_${Date.now()}`;
+
+    let fileUpload = bucket.file(newFileName);
+
+    const blobStream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.mimetype
+      }
+    });
+
+    blobStream.on('error', (error) => {
+      reject('Something is wrong! Unable to upload at the moment.');
+    });
+
+    blobStream.on('finish', () => {
+      // The public URL can be used to directly access the file via HTTP.
+      const url = format(`https://storage.googleapis.com/${bucket.name}/${groupId}/fullres/${fileUpload.name}`);
+      resolve(url);
+      console.log(url);
+    });
+
+    blobStream.end(file.buffer);
+  });
+  return prom;
+}
 
 // Start the server
 const PORT = process.env.PORT || 8080;
